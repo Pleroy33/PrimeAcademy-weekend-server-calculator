363a443e483843191e76a2460a536407c07b061b
Paul LeRoy <Pleroy33@gmail.com>
2023-12-09 18:27:23 -0600

    added post and get endpoints and tested in Postman

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,47 @@
-console.log('client.js is sourced!');
+function onReady(){
+    console.log('client.js is sourced!');//is this thing on?
+}
+
+function handlerGetCaluculations() {
+    axios({
+        method: "GET",
+        url: "/calculations"
+    }).then((response)=> {
+        console.log(' get is successful!!', response.data);
+  
+    })
+    .catch((error) =>{
+        console.log("server error :(", error);
+  
+    })
+  }
+
+function handlePostRequest(event) {
+    console.log("handlePostSubmit has been clicked");
+
+let numOne = document.getElementById('number1').value;
+let numTwo = document.getElementById('number2').value;
+let operator = document.getElementById('').value;
+ 
+
+    axios({
+    method: 'POST',
+    url: './calculations',
+    data: {
+        numOne: 10,    // 👈 number
+        numTwo: 5,     // 👈 number
+        operator: '', // 👈 string
+        }
+})
+.then((response) =>{
+    console.log('successfully Added' )
+})
+.catch((error) =>{
+    console.log('error', error)
+    
+})
+};
+
+
+
+
--- server/server.js
+++ server/server.js
@@ -1,27 +1,63 @@
+// Import Express module.
 const express = require('express');
-const app = express();
+
+// Import Body-parser module, essential for handling POST request data.
+const bodyParser = require('body-parser');
+
+//  Initialize the Express server.
+const app = express(); 
+
+app.use(bodyParser.urlencoded({extended: true}));
+// Required for Axios
+app.use(bodyParser.json())
+
+
 let PORT = process.env.PORT || 5001;
 
 app.use(express.json());
+// ! Set up the server to serve static files from 'server/public' directory.
 app.use(express.static('server/public'));
 
-// Global variable that will contain all of the
-// calculation objects:
+// ! Global variable that will contain all of the
+// ! calculation objects:
 let calculations = []
 
 
-// Here's a wonderful place to make some routes:
+// ! Here's a wonderful place to make some routes:
 
-// GET /calculations
+// ! GET /calculations
+app.get('/calculations', (req, res) => {
 
-// POST /calculations
+  // When creating an endpoint first do a console log.
+  console.log("Request for GET calculations was made")
+
+  // Responding will stop the code from running, similar to a return
+  res.sendStatus(200)
+  res.send()
+})
+
+
+// ! POST /calculations
+app.post('/calculations', (rec, res) => {
+
+  console.log("POST recieved from client:", rec.body);// console log incoming inputs and operator
+  console.log("addCalculation before push:", calculations);//console.log calculations before push 
+  //doCalculations(rec.body);//run the logic function that will do the calculating with inputs and operator.
+  console.log(rec.body);//console log the output of the logic function
+  calculations.push(rec.body);//push the output into the  calculations array
+  console.log("calculations after push:", calculations);//console log again to see that incoming guesses made it to addGuess
+  res.sendStatus(201);//send http code for succes back to client
+
+})
+
+
 
 
 // PLEASE DO NOT MODIFY ANY CODE BELOW THESE BEARS:
 // 🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸
 
 // Makes it so you don't have to kill the server
-// on 5000 in order to run the tests:
+// on 5001 in order to run the tests:
 if (process.env.NODE_ENV === 'test') {
   PORT = 5001;
 }
